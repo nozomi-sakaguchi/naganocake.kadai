600b5600b8183e44315ea89bd4cf0fb1e2585744
nozomi-sakaguchi <you@example.com>
2023-04-05 10:35:49 +0000

    1

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  namespace :admin do
+    get 'homes/top'
+  end
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
